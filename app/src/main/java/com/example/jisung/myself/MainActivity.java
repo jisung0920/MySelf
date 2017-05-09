5433067bbeab1213fdce94c935371f1d0ee196db
jisung <jisung0920@gmail.com>
2017-05-09 15:37:56 +0900

    layout

--- app/src/main/java/com/example/jisung/myself/MainActivity.java
+++ app/src/main/java/com/example/jisung/myself/MainActivity.java
@@ -6,25 +6,32 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.GridView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.zip.Inflater;
 
 public class MainActivity extends AppCompatActivity {
-    ListView list;
+    GridView list;
     todoAdapter adapter;
     ArrayList<toDo> todo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        list = (ListView)findViewById(R.id.list);
+        list = (GridView)findViewById(R.id.list);
         todo = new ArrayList<toDo>();
         todo.add(new toDo("멀미","asdf","afsd",true,false));
         todo.add(new toDo("컴구","asdf","afsd",true,false));
         todo.add(new toDo("디비","asdf","afsd",true,false));
         todo.add(new toDo("데통","asdf","afsd",true,false));
+        todo.add(new toDo("데통","asdf","afsd",true,false));
+        todo.add(new toDo("데통","asdf","afsd",true,false));
+        todo.add(new toDo("데통","asdf","afsd",true,false));
+        todo.add(new toDo("데통","asdf","afsd",true,false));
+        todo.add(new toDo("데통","asdf","afsd",true,false));
+
         adapter = new todoAdapter(this,todo);
         list.setAdapter(adapter);
         list.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
